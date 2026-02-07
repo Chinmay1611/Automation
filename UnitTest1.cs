5c0015284ce8e72c43a7deb9278a5739a9e7fe2b
Chinmay <x23200197@student.ncirl.ie>
2026-02-07 19:22:11 +0000

    demo start

---
+++ UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace PlaywriteDemo;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        //Playwright 
+        using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+        });
+        //Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://www.google.com/");
+        await page.ClickAsync("text= Reject all");
+
+    }
+}
